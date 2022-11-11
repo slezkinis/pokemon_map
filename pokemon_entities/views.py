e269288b24eef81f43711ea0d0a18fe2470bddd0
slezkinis <slezkin.is@mail.ru>
2022-11-11 20:07:11 +0300

    Show pokemons on mainpage

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,6 +1,7 @@
 import folium
 import json
 
+from .models import Pokemon
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
@@ -40,11 +41,14 @@ def show_all_pokemons(request):
             )
 
     pokemons_on_page = []
+    pokemons = Pokemon.objects.all()
     for pokemon in pokemons:
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': request.build_absolute_uri(
+                f'media/{pokemon.photo}'
+            ),
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
