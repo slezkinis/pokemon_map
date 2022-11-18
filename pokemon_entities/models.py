42bb517c5babcada0b1d5f0772b60bc2c00d839c
slezkinis <slezkin.is@mail.ru>
2022-11-18 19:48:07 +0300

    Add optional arguments

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(verbose_name='Имя покемона', max_length=200)
-    photo = models.ImageField(verbose_name='Картинка покемона', null=True)
+    photo = models.ImageField(verbose_name='Картинка покемона', null=True, blank=True)
     previous_evolution = models.ForeignKey(
         'self',
         verbose_name='Прошлая эволюция покемона',
@@ -29,8 +29,8 @@ class PokemonEntity(models.Model):
     lon = models.FloatField(verbose_name='Геог. долгота', null=True)
     appeared_at = models.DateTimeField(verbose_name='Во сколько появится')
     disappeared_at = models.DateTimeField(verbose_name='Во сколько пропадёт')
-    level = models.IntegerField(verbose_name='Уровень', default=1)
-    health = models.IntegerField(verbose_name='Здоровье', default=1)
-    strength = models.IntegerField(verbose_name='Сила', default=1)
-    defence = models.IntegerField(verbose_name='Защита', default=1)
-    stamina = models.IntegerField(verbose_name='Стамина', default=1)
+    level = models.IntegerField(verbose_name='Уровень', default=1, blank=True)
+    health = models.IntegerField(verbose_name='Здоровье', default=1, blank=True)
+    strength = models.IntegerField(verbose_name='Сила', default=1, blank=True)
+    defence = models.IntegerField(verbose_name='Защита', default=1, blank=True)
+    stamina = models.IntegerField(verbose_name='Стамина', default=1, blank=True)
