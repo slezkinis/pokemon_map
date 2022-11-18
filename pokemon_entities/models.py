f7813bb95876cf06fdd16096a23b0a3c32a48c1d
slezkinis <slezkin.is@mail.ru>
2022-11-18 19:37:16 +0300

    Add verbose name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,23 +2,35 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    photo = models.ImageField(null=True)
-    previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='next_evolutions')
-    description = models.CharField(blank=True, max_length=1000)
-    title_en = models.CharField(blank=True, max_length=200)
-    title_jp = models.CharField(max_length=200, blank=True)
+    title = models.CharField(verbose_name='Имя покемона', max_length=200)
+    photo = models.ImageField(verbose_name='Картинка покемона', null=True)
+    previous_evolution = models.ForeignKey(
+        'self',
+        verbose_name='Прошлая эволюция покемона',
+        on_delete=models.SET_NULL,
+        null=True, 
+        blank=True, 
+        related_name='next_evolutions',
+    )
+    description = models.CharField(verbose_name='Описание покемона', blank=True, max_length=1000)
+    title_en = models.CharField(verbose_name='Имя покемона на английском', blank=True, max_length=200)
+    title_jp = models.CharField(verbose_name='Имя покемона на японском', max_length=200, blank=True)
+
     def __str__(self) -> str:
         return self.title
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField(null=True)
-    lon = models.FloatField(null=True)
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField(default=1)
-    health = models.IntegerField(default=1)
-    strength = models.IntegerField(default=1)
-    defence = models.IntegerField(default=1)
-    stamina = models.IntegerField(default=1)
+    pokemon = models.ForeignKey(
+        Pokemon,
+        verbose_name='К кому привязано местоположения',
+        on_delete=models.CASCADE
+    )
+    lat = models.FloatField(verbose_name='Геог. ширина', null=True)
+    lon = models.FloatField(verbose_name='Геог. долгота', null=True)
+    appeared_at = models.DateTimeField(verbose_name='Во сколько появится')
+    disappeared_at = models.DateTimeField(verbose_name='Во сколько пропадёт')
+    level = models.IntegerField(verbose_name='Уровень', default=1)
+    health = models.IntegerField(verbose_name='Здоровье', default=1)
+    strength = models.IntegerField(verbose_name='Сила', default=1)
+    defence = models.IntegerField(verbose_name='Защита', default=1)
+    stamina = models.IntegerField(verbose_name='Стамина', default=1)
